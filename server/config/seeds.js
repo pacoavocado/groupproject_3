36bd57e46136a34aea59acde6803047413667436
Tina Pham <tina.pham8912@gmail.com>
2021-10-12 11:21:05 -0700

    edited price and quantity

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,8 +23,8 @@ db.once('open', async () => {
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'supermanButt.gif',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 10.25,
+      quantity: 100
     },
     {
       name: 'Canned Coffee',
@@ -32,8 +32,8 @@ db.once('open', async () => {
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
       image: 'jellostatue.gif',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 25.00,
+      quantity: 20
     },
     {
       name: 'Toilet Paper',
@@ -41,8 +41,8 @@ db.once('open', async () => {
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'openDoor.gif',
-      price: 7.99,
-      quantity: 20
+      price: 17.99,
+      quantity: 10
     },
     {
       name: 'Handmade Soap',
@@ -50,7 +50,7 @@ db.once('open', async () => {
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'skulls.gif',
-      price: 3.99,
+      price: 30.25,
       quantity: 50
     },
     {
@@ -59,8 +59,8 @@ db.once('open', async () => {
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'randompyramidHead.gif',
-      price: 14.99,
-      quantity: 100
+      price: 15.99,
+      quantity: 20
     },
     {
       name: 'Camera',
@@ -68,8 +68,8 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'moreMountains.gif',
-      price: 399.99,
-      quantity: 30
+      price: 25.00,
+      quantity: 10
     },
     {
       name: 'Tablet',
@@ -77,8 +77,8 @@ db.once('open', async () => {
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'hugging.gif',
-      price: 199.99,
-      quantity: 30
+      price: 15.99,
+      quantity: 10
     },
     {
       name: 'Tales at Bedtime',
@@ -103,8 +103,8 @@ db.once('open', async () => {
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
       image: 'head.gif',
-      price: 2.99,
-      quantity: 1000
+      price: 22.99,
+      quantity: 5
     },
     {
       name: 'Teddy Bear',
@@ -112,8 +112,8 @@ db.once('open', async () => {
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'lightingWand.gif',
-      price: 7.99,
-      quantity: 100
+      price: 97.99,
+      quantity: 2
     },
     {
       name: 'Alphabet Blocks',
@@ -121,8 +121,8 @@ db.once('open', async () => {
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'cityView.gif',
-      price: 9.99,
-      quantity: 600
+      price: 5.99,
+      quantity: 5
     }
   ]);
 
